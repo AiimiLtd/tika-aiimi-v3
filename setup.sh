00f682547200c8b93a7a2f23a8aa36889fdf45cf
Paul Clark <paul@piclark.uk>
2025-02-20 12:23:55 +0000

    Release and Build scripts

---
+++ setup.sh
@@ -0,0 +1,12 @@
+# maven='3.9.9'
+sudo apt install openjdk-21-jdk-headless openjdk-21-jre-headless maven -y
+# cd ~
+# [ -d .maven ] || mkdir .maven 
+# cd .maven 
+# wget "https://dlcdn.apache.org/maven/maven-3/${maven}/binaries/apache-maven-${maven}-bin.tar.gz"
+# tar xzvf "apache-maven-${maven}-bin.tar.gz"
+# cd "apache-maven-${maven}/bin"
+# path=$(pwd)
+# toadd="\"${path}:\$PATH\""
+# echo "PATH=${toadd}" >> ~/.profile
+# source ~/.profile
